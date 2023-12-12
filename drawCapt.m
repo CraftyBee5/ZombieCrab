da117ddb8d64e9c4f127dcfcefe05e18a0b6525c
graceputn <gp141@humboldt.edu>
2023-12-12 04:13:55 -0800

    capt points

--- drawCapt.m
+++ drawCapt.m
@@ -12,9 +12,17 @@ capt = getCapt(sizeCapt, xCapt, yCapt, thetaCapt);
 % TODO : Shift the captain from (0 , 0) to (xCapt , yCapt)
 % Extract the captain points from the captain matrix capt.
 
+<<<<<<< Updated upstream
 % rotate captain
 R = getRotation(thetaCapt);
 %R = 1;
+=======
+capt = getCapt(sizeCapt, xCapt, yCapt);
+
+% rotate captain
+R = getRotation(thetaCapt);
+R = 1;
+>>>>>>> Stashed changes
 captRotated = R*capt;
 
 % shift captain to new location
@@ -41,6 +49,13 @@ pt17=capt( : , 17);
 pt18=capt( : , 18);
 pt19=capt( : , 19);
 pt20=capt( : , 20);
+pt21=capt( : , 21);
+pt22=capt( : , 22);
+pt23=capt( : , 23);
+pt24=capt( : , 24);
+pt25=capt( : , 25);
+pt26=capt( : , 26);
+pt27=capt( : , 27);
 
 % Draw the captain and set the return vector of graphics handles.
 captainGraphics(1) = drawLine(pt1 , pt2 , "k");
@@ -50,6 +65,7 @@ captainGraphics(4) = drawLine(pt4 , pt5 , "k");
 captainGraphics(5) = drawLine(pt5 , pt6 , "k");
 captainGraphics(6) = drawLine(pt6 , pt1 , "k");
 captainGraphics(7) = drawLine(pt7 , pt8 , "k");
+<<<<<<< Updated upstream
 captainGraphics(8) = drawLine(pt8 , pt9 , "k");
 captainGraphics(9) = drawLine(pt9 , pt10 , "k");
 captainGraphics(10) = drawLine(pt1 , pt11 , "k");
@@ -65,6 +81,32 @@ net = pt19;
 
 xNet = net(1);  % Extract the x-value
 yNet = net(2);  % Extract the y-value
+=======
+captainGraphics(8) = drawLine(pt6 , pt8 , "k");
+captainGraphics(9) = drawLine(pt4 , pt9 , "k");
+captainGraphics(10) = drawLine(pt9 , pt10 , "k");
+captainGraphics(11) = drawLine(pt10 , pt6 , "k");
+captainGraphics(12) = drawLine(pt5 , pt11 , "k");
+captainGraphics(13) = drawLine(pt11 , pt12 , "k");
+captainGraphics(14) = drawLine(pt12 , pt7 , "k");
+captainGraphics(15) = drawLine(pt7 , pt13 , "k");
+captainGraphics(16) = drawLine(pt13 , pt14 , "k");
+captainGraphics(17) = drawLine(pt14 , pt15 , "k");
+captainGraphics(18) = drawLine(pt15 , pt16 , "k");
+captainGraphics(19) = drawLine(pt17 , pt18 , "k");
+captainGraphics(20) = drawLine(pt18 , pt19 , "k");
+captainGraphics(21) = drawLine(pt19 , pt20 , "k");
+captainGraphics(22) = drawLine(pt20 , pt6 , "k");
+captainGraphics(23) = drawLine(pt20 , pt13 , "k");
+captainGraphics(24) = drawLine(pt21 , pt22 , "k");
+captainGraphics(25) = drawLine(pt22 , pt23 , "k");
+captainGraphics(26) = drawLine(pt23 , pt12 , "k");
+captainGraphics(27) = drawLine(pt10 , pt24 , "k");
+captainGraphics(28) = drawLine(pt6 , pt25, "k");
+captainGraphics(29) = drawLine(pt25 , pt26 , "k");
+captainGraphics(30) = drawLine(pt26 , pt24 , "k");
+captainGraphics(31) = drawLine(pt24 , pt27 , "k");
+>>>>>>> Stashed changes
 
 endfunction
 
--- getCapt.m
+++ getCapt.m
@@ -1,17 +1,15 @@
 function capt = getCapt (captSize, xCapt, yCapt, thetaCapt)
 
-
+  %captains head
   captPt1 = [ 2*captSize; 0; 1]; % center top of head
   captPt2 = [ 1.75*captSize; -0.25*captSize; 1]; % right top of head
   captPt3 = [ 1.75*captSize; 0.25*captSize; 1]; % left top of head
-
   captPt4 = [ 1.5*captSize; -0.5*captSize; 1]; % right center side of head
   captPt5 = [ 1.5*captSize; 0.5*captSize; 1]; % left center side of head
-
   captPt6 = [ 1.25*captSize; -0.25*captSize; 1]; % right bottom of head
   captPt7 = [ 1.25*captSize; 0.25*captSize; 1]; % left bottom of head
-
   captPt8 = [ captSize; 0; 1]; % center bottom of head
+<<<<<<< Updated upstream
 
   captPt9 = [ 1.5*captSize; -0.75*captSize; 1]; % right top of shoulder
   captPt10 = [ 1.25*captSize; -0.75*captSize; 1]; % right bottom of shoulder
@@ -33,42 +31,33 @@ function capt = getCapt (captSize, xCapt, yCapt, thetaCapt)
 
   captPt19 = [ -0.25*captSize; -0.25*captSize; 1]; % left foot
   captPt20 = [ captSize; -0.25*captSize; 1]; % right hip
+=======
+  %captains shoulders
+  captPt9 = [ 1.5*captSize; -0.75*captSize; 1]; % right top of shoulder
+  captPt10 = [ 1.25*captSize; -0.75*captSize; 1]; % right bottom of shoulder
+  captPt11 = [ 1.5*captSize; 0.75*captSize; 1]; % left top of shoulder
+  captPt12 = [ 1.25*captSize; 0.75*captSize; 1]; % left bottom of shoulder
+  %captains body and legs
+  captPt13 = [ 0; 0.25*captSize; 1]; % left hip
+  captPt14 = [ -0.25*captSize; 0.25*captSize; 1]; % right foot
+  captPt15 = [ -0.25*aptSize; 0.15*captSize; 1]; % right foot
+  captPt16 = [ 0; 0.15*captSize; 1]; % inside right hip
+  captPt17 = [ 0; -0.15*captSize; 1]; % inside left hip
+  captPt18 = [ -0.25*captSize; -0.15*captSize; 1]; % left foot
+  captPt19 = [ -0.25*captSize; -0.25*captSize; 1]; % left foot
+  captPt20 = [ 0; -0.25*captSize; 1]; % right hip
+  %captains arms
+  captPt21 = [ 1.25*captSize; 0.5*captSize; 1]; % top of left arm
+  captPt22 = [ 0; 0.5*captSize; 1]; % bottom of left arm 
+  captPt23 = [ 0; 0.5*captSize; 1]; % bottom of left arm
+  captPt24 = [ 0; -0.5*captSize; 1]; % bottom of right arm
+  %captains weapon
+  captPt25 = [ -1.25captSize; -0.35*captSize; 1]; % base of weapon
+  captPt26 = [ -1.25captSize; -0.5*captSize; 1]; % base of weapon
+  captPt27 = [ 1.25captSize; -0.5*captSize; 1]; % top of weapon
+>>>>>>> Stashed changes
 
-##%left side
-##captPt1 = [captSize; captSize; 1];
-##captPt2 = [-captSize; captSize; 1];
-##captPt3 = [-3 * captSize; captSize; 1];
-##
-##%right side
-##captPt4 = [-3 * captSize; -captSize; 1];
-##captPt5 = [-captSize; -captSize; 1];
-##captPt6 = [captSize; -captSize; 1];
-##
-##%head
-##captPt7 = [ captSize; -captSize/2; 1]; %right neck
-##captPt8 = [ 2 *captSize; -captSize/2; 1]; %right top of head
-##captPt9 = [ 2*captSize; captSize/2; 1]; %left top of head
-##captPt10 = [ captSize; captSize/2; 1]; %left neck
-##
-##%hands
-##captPt11 = [ 0; 2*captSize; 1]; %left
-##captPt12 = [ 0; -2*captSize; 1]; %right
-##
-##%spear
-##captPt13=[ 2*captSize;-2 * captSize; 1]; % spear point
-##captPt14=[ -captSize; -2*captSize; 1]; % spear end
-##
-##%net
-##captPt15=[ 2*captSize; -captSize; 1 ];
-##captPt16=[ 2*captSize; -3*captSize; 1 ];
-##captPt17=[ 4*captSize; -captSize; 1 ];
-##captPt18=[ 4*captSize; -3*captSize; 1 ];
-##
-##%middle of net
-##captPt19=[ 3*captSize; -2*captSize; 1 ];
-##captPt20=[ 3*captSize + 1; -2*captSize + 1; 1 ];
-
-  %captain matrix
-  capt = [ captPt1, captPt2, captPt3, captPt4, captPt5, captPt6, captPt7, captPt8, captPt9, captPt10, captPt11, captPt12, captPt13, captPt14,  captPt15, captPt16, captPt17, captPt18, captPt19, captPt20];
+  %captains matrix
+  capt = [ captPt1, captPt2, captPt3, captPt4, captPt5, captPt6, captPt7, captPt8, captPt9, captPt10, captPt11, captPt12, captPt13, captPt14,  captPt15, captPt16, captPt17, captPt18, captPt19, captPt20, captPt21, captPt22, captPt23, captPt24, captPt25, captPt26, captPt27];
 
 endfunction
