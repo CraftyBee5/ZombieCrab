39f60d110c99879eb0e694d3d346b51cded7825f
CraftyBee5 <ajb254@humboldt.edu>
2023-12-11 19:22:21 -0800

    guns

--- play.m
+++ play.m
@@ -20,6 +20,7 @@ while (1)
 
   %testing out weapons
   gun1 = drawGun1 (400, 400, -pi/2, 10);
+  gun2 = drawGun2 (200, 400, -pi/2, 10);
 
   %infoText = text(10, mapHeight - 10, sprintf('Wave %d Lives %d', wave, lives), 'Color', 'white', 'FontSize', 12);
 
