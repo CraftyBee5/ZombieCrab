5ca39daafbebf5be81864cc43c34308446fbfde8
CornCobb83 <jordancobb66@gmail.com>
2023-12-11 20:37:05 -0800

    bullet

---
+++ equipGun.m
@@ -0,0 +1,11 @@
+function bulletSpeed = equipGun (gunName)
+
+  if (gunName == 'pistol')
+    bulletSpeed = 3;
+  elseif (gunName == 'rifle')
+    bulletSpeed = 5;
+  elseif (gunName == 'wunderwaffe')
+    bulletSpeed = 5;
+  endif
+
+endfunction
--- play.m
+++ play.m
@@ -3,6 +3,20 @@ function play ()
 numCrabs = 2;
 wave = 1;
 lives = 10;
+crabsDefeated = 3;
+
+rifle = false;
+pistol = false;
+
+shotsFired = 0;
+bulletSpeed = 2;
+
+g1X = 0;
+g1Y = 0;
+g2X = 0;
+g2Y = 0;
+bX = 0;
+bY = 0;
 
 [mapHeight , mapWidth] = drawMap( "Beach.jpg");
 
@@ -16,15 +30,35 @@ charX = 300;
 charY = 150;
 character = drawCapt (charX, charY, -pi/2, 20);
 
-bX = 100;
-bY = 100;
-bullet = drawBullet (bX, bY, -pi/2, 2);
-
 while (1)
 
-  %testing out weapons
-  gun1 = drawGun1 (400, 400, 0, 10);
-  gun2 = drawGun2 (200, 400, 0, 10);
+  if (crabsDefeated >= 2 && !pistol)
+    pistol = true;
+    g2X = randi(400) + 100;
+    g2Y = randi(100) + 200;
+    gun2 = drawGun2 (g2X, g2Y, 0, 10);
+  endif
+
+  if ((sqrt((charX - g2X)^2 + ((charY - g2Y)^2))) < 10 && pistol)
+    bulletSpeed = equipGun('pistol');
+    if (isgraphics(gun2))
+      delete(gun2);
+    endif
+  endif
+
+  if (crabsDefeated >= 8 && !rifle)
+    rifle = true
+    g1X = randi(400) + 100;
+    g1Y = randi(100) + 200;
+    gun1 = drawGun1 (g1X, g1Y, 0, 10);
+  endif
+
+  if ((sqrt((charX - g1X)^2 + ((charY - g1Y)^2))) < 10 && rifle)
+    bulletSpeed = equipGun('rifle');
+    if (isgraphics(gun1))
+      delete(gun1);
+    endif
+  endif
 
   %infoText = text(10, mapHeight - 10, sprintf('Wave %d Lives %d', wave, lives), 'Color', 'white', 'FontSize', 12);
 
@@ -60,6 +94,24 @@ while (1)
     charX += 10;
     character = drawCapt (charX, charY, -pi/2, 20);
   endif
+
+    if (cmd == " ")
+        shotsFired += 1;
+        bX(shotsFired) = charX;
+        bY(shotsFired) = charY;
+    endif
+
+    if (shotsFired > 0)
+        for i = 1:shotsFired
+            bY(i) = bY(i) + bulletSpeed; % Move bullets upward
+            bullet{i} = drawBullet(bX(i), bY(i), -pi/2, 2);
+
+            % Check if bullet reached the top of the map
+            if bY(i) > mapHeight
+                delete(bullet{i});
+            endif
+        endfor
+    endif
 
   %Movement of Crabs
   for i = 1 : numCrabs
